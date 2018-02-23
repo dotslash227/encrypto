8f69a9aa17e488fdd6ce5bc5e09b6bfe20a40a9b
Abhishek Biswal <wirddin@gmail.com>
2018-02-23 13:51:51 +0530

    App Intro : init

--- app/config/router.js
+++ app/config/router.js
@@ -2,14 +2,18 @@ import { StackNavigator } from 'react-navigation';
 
 // Screens:
 import HomeScreen from "../screens/Home";
+import IntroScreen from "../screens/Intro";
 
 export default StackNavigator({
   	Home: {
   	  screen: HomeScreen,
+    },
+    Intro: {
+  	  screen: IntroScreen,
   	},
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Intro',
     navigationOptions: {
     	header: null
     }
---
+++ app/screens/Intro.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 320
+  },
+  text: {
+    color: '#333',
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 22,
+    color: '#000000',
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    marginBottom: 16,
+  }
+});
+
+const slides = [
+  {
+    key: "somethun",
+    title: "Title 1",
+    text: "Description.\nSay something cool",
+    image: require("./../assets/1.jpg"),
+    imageStyle: styles.image,
+	backgroundColor: "#E1E1E1",
+	textStyle: styles.text,
+	titleStyle: styles.title,
+  },
+  {
+    key: "somethun-dos",
+    title: "Title 2",
+    text: "Other cool stuff",
+    image: require("./../assets/1.jpg"),
+    imageStyle: styles.image,
+	backgroundColor: "#E1E1E1",
+	textStyle: styles.text,
+	titleStyle: styles.title,
+  },
+  {
+    key: "somethun1",
+    title: "Rocket guy",
+    text: "I'm already out of descriptions\n\nLorem ipsum bla bla bla",
+    image: require("./../assets/1.jpg"),
+    imageStyle: styles.image,
+	backgroundColor: "#E1E1E1",
+	textStyle: styles.text,
+	titleStyle: styles.title,
+  }
+];
+
+export default class Intro extends Component {
+  _onDone = () => {
+	this.props.navigation.navigate('Home');
+  };
+  render() {
+	return <AppIntroSlider
+		slides={slides}
+		onDone={this._onDone}
+		showSkipButton={true}
+		/>;
+  }
+}
