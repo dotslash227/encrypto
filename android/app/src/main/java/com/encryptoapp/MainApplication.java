d810681c4063253c3f171d0296634a28ffc67f3d
Abhishek Biswal <wirddin@gmail.com>
2018-04-09 00:45:50 +0530

    Installed AccountKit + config for Android AK

--- android/app/src/main/java/com/encryptoapp/MainApplication.java
+++ android/app/src/main/java/com/encryptoapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.encryptoapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.underscope.react.fbak.RNAccountKitPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAccountKitPackage(),
             new FBSDKPackage(mCallbackManager),
             new SvgPackage()
       );
