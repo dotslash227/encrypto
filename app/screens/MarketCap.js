c702f7210947a781689933b897241af00cc66168
Abhishek Biswal <wirddin@gmail.com>
2018-05-27 21:31:49 +0530

    MarketCap Probable Fix

--- app/screens/MarketCap.js
+++ app/screens/MarketCap.js
@@ -1,12 +1,7 @@
-import React, {
-	Component
-} from "react";
-import {
-	View,
-	StyleSheet
-} from "react-native";
+//
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
 import Header from "../components/Header";
-import axios from "axios";
 import {
 	Container,
 	Content,
@@ -22,8 +17,6 @@ import {
 	Thumbnail
 } from "native-base";
 
-import config from "../config.json";
-
 export default class MarketCap extends Component {
 	constructor(props) {
 		super(props);
@@ -34,135 +27,77 @@ export default class MarketCap extends Component {
 		};
 	}
 
-	componentDidMount = () => {
-		var t = this;
-		const fetchUrl = config.api.base + "/api/data/marketcap";
-		axios(fetchUrl)
-			.then(res => res.data)
+	componentDidMount() {
+		fetch("https://api.coinmarketcap.com/v1/ticker/?convert=INR&limit=20", {
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => response.json())
 			.then(response => {
-				/* console.log({ response });
-				alert("Res", response);
-				alert("Res str", response.toString());
-				alert("T", t); */
-				if (response) {
-					t.setState({
-						loading: false,
-						ticker: response
-					});
-				} else {
-					t.setState({
-						loading: false,
-						error: "Something went wrong."
-					});
-				}
+				this.setState({ loading: false, ticker: response });
 			})
 			.catch(e => {
 				console.log(e);
-				t.setState({
-					loading: false,
-					error: "Something went wrong." + e
-				});
+				this.setState({ loading: false, error: "Something went wrong." });
 			});
-	};
+	}
 
 	render() {
-		const {
-			ticker,
-			loading,
-			error
-		} = this.state;
+		const { ticker, loading, error } = this.state;
+		let content;
 		if (loading) {
-			var content = < Spinner / > ;
-		} else if (error) {
-			var content = < Text > {
-				error
-			} < /Text>;
+			content = <Spinner />;
 		} else {
-			var content = < ListOfCoins ticker = {
-				ticker
-			}
-			/>;
+			content = (<ListOfCoins ticker={ticker} />);
 		}
-		return ( <
-			Container >
-			<
-			Header { ...this.props
-			}
-			title = "Market Cap" / >
-			<
-			Content > {
-				content
-			} < /Content> <
-			/Container>
+		return (
+			<Container>
+				<Header {...this.props} title="Market Cap" />
+				<Content>{content}</Content>
+			</Container>
 		);
 	}
 }
 
 class ListOfCoins extends Component {
 	render() {
-		const {
-			ticker
-		} = this.props;
+		const { ticker } = this.props;
 		let list = ticker.map(ticker => {
 			let percentage;
 			if (ticker.percent_change_24h.indexOf("-") > -1) {
-				percentage = ( <
-					Text style = {
-						[styles.percentage, styles.down]
-					} > {
-						ticker.percent_change_24h
-					} <
-					/Text>
+				percentage = (
+					<Text style={[styles.percentage, styles.down]}>
+						{ticker.percent_change_24h}
+					</Text>
 				);
 			} else {
-				percentage = ( <
-					Text style = {
-						[styles.percentage, styles.up]
-					} > {
-						ticker.percent_change_24h
-					} <
-					/Text>
+				percentage = (
+					<Text style={[styles.percentage, styles.up]}>
+						{ticker.percent_change_24h}
+					</Text>
 				);
 			}
-			return ( <
-				ListItem key = {
-					ticker.id
-				} >
-				<
-				Thumbnail size = {
-					32
-				}
-				source = {
-					{
-						uri: `https://raw.githubusercontent.com/cjdowner/cryptocurrency-icons/master/32/color/${ticker.symbol.toLowerCase()}.png`
-					}
-				}
-				/> <
-				Body >
-				<
-				Text > {
-					ticker.name
-				} < /Text> <
-				Text note > {
-					ticker.price_inr
-				}
-				INR < /Text> <
-				/Body> <
-				Right >
-				<
-				Text note > {
-					percentage
-				} < /Text> <
-				/Right> <
-				/ListItem>
+			return (
+				<ListItem key={ticker.id}>
+					<Thumbnail
+						square
+						size={80}
+						source={{ uri: "http://via.placeholder.com/80x80" }}
+					/>
+					<Body>
+						<Text>{ticker.name}</Text>
+						<Text note>{ticker.price_inr} INR</Text>
+					</Body>
+					<Right>
+						<Text note>{percentage}</Text>
+					</Right>
+				</ListItem>
 			);
 		});
-		console.log({
-			list
-		});
-		return <List > {
-			list
-		} < /List>;
+		return <List>{list}</List>;
 	}
 }
 
