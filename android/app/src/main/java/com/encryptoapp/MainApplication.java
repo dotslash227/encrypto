128ed6914ed08bc9bbe6d5d8d2952f73ce5c033b
Abhishek Biswal <wirddin@gmail.com>
2018-07-03 18:32:04 +0530

    Charts wrapper for MP Charts

--- android/app/src/main/java/com/encryptoapp/MainApplication.java
+++ android/app/src/main/java/com/encryptoapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.encryptoapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.wuxudong.rncharts.MPAndroidChartPackage;
 import io.fullstack.oauth.OAuthManagerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
@@ -40,6 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MPAndroidChartPackage(),
             new OAuthManagerPackage(),
             new VectorIconsPackage(),
             new ReactNativeOneSignalPackage(),
